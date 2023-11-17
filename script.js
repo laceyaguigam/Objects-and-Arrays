3ae19e47a96043839e71b6776a668e45d7cc0bcb
lacey aguigam <laceyaguigam@yahoo.com>
2023-11-16 19:28:10 -0800

    realized start was undefined, change i = 0 to i = start, however function still returns undefined

--- script.js
+++ script.js
@@ -6,8 +6,7 @@
 
 function range(start, end) {
     let rangeArray = [];
-   
-    for (let i = 0; i < end.length; i++) {
+    for (let i = start; i < end.length; i++) {
         rangeArray.push[i];
     }
         return rangeArray;
